02ada5be18ec7513864112badbdfa72370bf17dd
urmil <ucodes28@gmail.com>
2020-11-22 21:55:04 -0600

    image load into the image view using UI image property

--- StormViewerRepeat/DetailViewController.swift
+++ StormViewerRepeat/DetailViewController.swift
@@ -15,6 +15,10 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
     
 }
--- StormViewerRepeat/ViewController.swift
+++ StormViewerRepeat/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
-            
+            navigationController?.pushViewController(vc, animated: true)
         }
     }
     
